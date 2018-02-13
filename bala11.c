f5e70a9a8d8fda0cc40965027cfc2d873844c4c3
alab9626023145 <35652494+alab9626023145@users.noreply.github.com>
2018-02-13 16:45:09 +0530

    Create bala11.c

---
+++ bala11.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+num=int(input())
+if num>1:
+	for i in range(2,num):
+		if(num%i==0):
+			print(num,"the number is not prime")
+			break
+	else:
+			print(num,"the number is  prime")
+else:
+	print("the number is not prime")
+
+   }
