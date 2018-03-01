d54675f853a9520dad06d7912e7af55d887187bb
alab9626023145 <35652494+alab9626023145@users.noreply.github.com>
2018-03-01 16:42:02 +0530

    Create bala18.c

---
+++ bala18.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int i,n;
+    printf("enter n value");
+    scanf("%d",&n);
+   printf("multiplication table\n");
+     for(i=1;i<=n;i++)
+    {
+   printf("multiple the number %d \n ",(i*5));
+ 
+}
+}
